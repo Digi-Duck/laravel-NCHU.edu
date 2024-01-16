d6c8ea91f24353033bc6fd76f1a90ef8e40e00ba
pumpkin3225 <152354723+pumpkin3225@users.noreply.github.com>
2024-01-16 13:29:59 +0800

    create Laravel_Vue3_Tailwinds

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import Example from './components/ExampleComponent.vue';
+
+const app = createApp({
+  components: {
+    Example
+  }
+});
+
+app.mount('#app');
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,18 @@
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Vue產的一路狂飆'
+        };
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1 class="bg-[red] text-[50px]">{{ message }}</h1>
+    </div>
+</template>
+
+<style scoped></style>
