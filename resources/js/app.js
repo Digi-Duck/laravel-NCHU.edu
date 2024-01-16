57c410d837e56d2f83840c391c2b2ed4dc521270
pumpkin3225 <hank106106@gmail.com>
2024-01-16 21:34:57 +0800

    remove: bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import Example from './components/ExampleComponent.vue';
 import Banner from './components/Banner.vue';
 
+
 const app = createApp({
   components: {
     Example, Banner,
