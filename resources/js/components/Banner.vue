b3bd95691a6f78209d20103810725430c4a3641f
pumpkin3225 <hank106106@gmail.com>
2024-01-16 21:46:47 +0800

    create: FontAwesome

--- resources/js/components/Banner.vue
+++ resources/js/components/Banner.vue
@@ -1,25 +1,28 @@
+
 <template>
     <div class="container">
-    <div class="font-185">創新教育</div>
-    <div class="font-185">開啟無限可能</div>
-    <div class="font-80">Innovative education, </div>
-    <div class="font-80">unlocking limitless</div>
-    <div class=""></div>
-    <div class=""><img src="" alt=""></div>
-</div>
+        <div class="font-185">創新教育</div>
+        <div class="font-185">開啟無限可能</div>
+        <div class="font-80">Innovative education, </div>
+        <div class="font-80">unlocking limitless</div>
+        <div class=""></div>
+        <div class=""><img src="" alt=""></div>
+    </div>
 </template>
 <style scoped>
-.test{
+.test {
     width: 300px;
     height: 300px;
     background-image: url('img');
 }
-.font-185{
+
+.font-185 {
     @apply text-[185px] text-[#0057ff];
 }
-.font-80{
-    @apply text-[80px]  text-[#000000];
-}
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@800&display=swap');
 
+.font-80 {
+    @apply text-[80px] text-[#000000];
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@800&display=swap');
 </style>
