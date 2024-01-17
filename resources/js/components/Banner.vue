4168262e8ac00bc29ae748826bc878b7a61c2628
pumpkin3225 <hank106106@gmail.com>
2024-01-17 20:21:07 +0800

    fix :banner_img

--- resources/js/components/Banner.vue
+++ resources/js/components/Banner.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <div class="w-full mb-[81px]">
+    <div class="w-full">
         <div class="flex">
             <div class="font-title ml-[120px] row-one ">創新教育</div>
         </div>
@@ -9,15 +9,15 @@
         <div class="font-large ml-[319px]  mt-[-30px] row-four">unlocking limitless</div>
         <div class="circle-p w-0 h-0"></div>
         <div class="right-p w-0 h-0"></div>
-        <div class="font-large ml-[812px] mt-[-45px] row-five">possibilities.</div>
-        <div class="flex ml-[415px] ">
+        <div class="font-large ml-[812px] mt-[-43px] row-five">possibilities.</div>
+        <div class="flex ml-[415px] mb-[90px]">
             <img class="mr-[53px] w-[90px] h-[90px]" src="/public/images/banner/black-row.png" alt="">
             <img class="mr-[53px] w-[90px] h-[90px]" src="/public/images/banner/black-row.png" alt="">
             <img class="mr-[53px] w-[90px] h-[90px]" src="/public/images/banner/black-row.png" alt="">
         </div>
-    </div>
-    <div class="w-full">
-        <img class="w-full" src="/public/images/banner/Rectangle 64.png" alt="">
+        <div class="w-[84%] m-auto">
+            <img class="w-full" src="/public/images/banner/Rectangle 64.png" alt="">
+        </div>
     </div>
 </template>
 <style scoped>
@@ -25,10 +25,11 @@
     @apply text-[145px] text-[#0057ff];
     font-family: 'Noto Sans TC', sans-serif;
     font-weight: 900;
+    z-index: 1;
 }
 
 .font-large {
-    @apply text-[64px] text-[#000000] font-black tracking-[-2.5px];
+    @apply text-[64px] text-[#000000] font-black tracking-[-3px];
     font-family: 'Inter', sans-serif;
     font-weight: 700;
 }
@@ -73,21 +74,20 @@
 .row-three:before {
     content: "";
     position: absolute;
-    left: -429px;
-    bottom: 23px;
+    left: -420px;
+    bottom: 15px;
     height: 201px;
     width: 549px;
     background-image: url('images/banner/left-p.png');
     background-repeat: no-repeat;
     background-position-x: left;
-    z-index: -1;
 }
 
 .row-five:before {
     content: "";
     position: absolute;
     left: -35px;
-    bottom: -28px;
+    bottom: -22px;
     height: 115px;
     width: 409px;
     background-image: url('images/banner/draw_circle.png');
@@ -97,13 +97,13 @@
 .right-p:after {
     content: "";
     position: absolute;
-    left: 1151px;
-    top: -53px;
+    left: 1142px;
+    top: -58px;
     height: 216px;
     width: 251px;
     background-image: url('images/banner/right-p.png');
     background-repeat: no-repeat;
-    z-index: -1;
+   ;
 }
 
 .circle-p:after {
@@ -113,7 +113,7 @@
     bottom: 248px;
     width: 166px;
     height: 166px;
-    z-index: -1;
+   ;
     background-image: url('images/banner/circle-p.png');
     background-repeat: no-repeat;
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>中興大學推廣教育</title>
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
 </head>
@@ -12,17 +12,21 @@
     .nav-bar {
         box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.1);
     }
+
+    .background {
+        background-image: url('/images/body/grid.svg');
+    }
 </style>
 
 <body>
-    <div id="app" class="w-[100%] ">
+    <div id="app" class="w-[100%] background">
         <nav class="nav-bar h-[60px] bg-white flex items-center justify-between px-[40px]">
             <Navbar />
         </nav>
-        <header class="h-[100vh] m-auto w-[80%] mt-[37px]">
+        <header class="h-[100vh] m-auto w-[80%] mt-[37px] ">
             <banner />
         </header>
-        <main class="m-auto w-[80%]"></main>
+        <main class="m-auto w-[80%] h-[100vh]"></main>
         <footer class="m-auto w-[80%]">
             <foot />
         </footer>
