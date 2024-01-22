5934e6c8ac072a551b3c76f381e5f11e2f79302a
pumpkin3225 <hank106106@gmail.com>
2024-01-22 23:26:56 +0800

    seeder : course_types

---
+++ database/seeders/course_types_table_seeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\CourseType;
+
+class course_types_table_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (CourseType::count() > 0) {
+            return;
+        }
+        CourseType::insert(
+            [
+                [
+                    'name' => '兒童營隊',
+                    'content' => '體驗平日上課較無機會接觸到的課程，讓小朋友有個難忘且充實的寒暑假。',
+                    'img_path' => '/main/camp.svg',
+                    'sort' => 0,
+                ],
+                [
+                    'name' => '單元手作',
+                    'content' => '短時間的學習課程，適合生活忙碌的、想要體驗不同領域或是培養其他興趣的你。',
+                    'img_path' => '/main/handmade.svg',
+                    'sort' => 1,
+                ],
+                [
+                    'name' => '運動課程',
+                    'content' => '為期2~3個月運動課程，適合想要運動、喜歡運動或是想挑戰不同以往運動的你。',
+                    'img_path' => '/main/sport.svg',
+                    'sort' => 2,
+                ],
+                [
+                    'name' => '職業訓練',
+                    'content' => '適合有較充裕時間，想要了解、學習、體驗更完整不同領域知識的你。',
+                    'img_path' => '/main/training.svg',
+                    'sort' => 3,
+                ],
+                [
+                    'name' => '專業證照',
+                    'content' => '適合想要考取證照的你，由課程專業講師帶領你了解各種專業知識，得到專業認證。',
+                    'img_path' => '/main/certificate.svg',
+                    'sort' => 4,
+                ],
+            ]
+        );
+    }
+}
