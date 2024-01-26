6f129c206584b0b141f165ae5cb859ac9de38e77
pumpkin3225 <152354723+pumpkin3225@users.noreply.github.com>
2024-01-26 14:02:31 +0800

    indexCourse to Vw :cry:

--- resources/js/Components/Frontend/Other/IndexCourse.vue
+++ resources/js/Components/Frontend/Other/IndexCourse.vue
@@ -42,72 +42,84 @@ export default {
 }
 </script>
 <template>
-    <div class="flex justify-center items-center ">
-        <section class=" w-[1294px] h-[100vh] flex justify-center items-center  overflow-hidden" @mouseleave="resetCards">
-            <div class="w-[776px] h-[650px]">
-                <div class="cards h-[468px] relative" @wheel.prevent="handleMouseWheel">
-                    <div v-for="card in cards" :key="card.id" class="card"
-                        :style="{ transform: card.transform, backgroundColor: card.color, opacity: card.opacity }">
-                        <a :href="card.link">
-                            <div class="card flex flex-col justify-end">
-                                <div class="card-type">{{ card.type }}</div>
-                                <div class="card-title">{{ card.title }}</div>
-                                <div class="card-tags flex">
-                                    <div v-for="tag in [card.tag1, card.tag2, card.tag3, card.tag4]" :key="tag.id"
-                                        class="card-tag">{{ tag }}</div>
+    <div class="w-[90%] md:w-[80%] m-auto">
+        <div class="flex justify-center items-center mb-[5%]">
+            <section class=" w-[67.39vw] py-[3%] flex justify-center items-center  overflow-hidden"
+                @mouseleave="resetCards">
+                <div class="w-[40.41vw] h-[33.85vw]">
+                    <div class="cards h-[24.37vw] relative" @wheel.prevent="handleMouseWheel">
+                        <div v-for="card in cards" :key="card.id" class="card"
+                            :style="{ transform: card.transform, backgroundColor: card.color, opacity: card.opacity }">
+                            <a :href="card.link">
+                                <div class="card flex flex-col justify-end">
+                                    <div class="card-type">{{ card.type }}</div>
+                                    <div class="card-title">{{ card.title }}</div>
+                                    <div class="card-tags flex">
+                                        <div v-for="tag in [card.tag1, card.tag2, card.tag3, card.tag4]" :key="tag.id"
+                                            class="card-tag">{{ tag }}</div>
+                                    </div>
                                 </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-[27.7vw] h-[34.59] ">
+                    <div class="mt-[-3.47vw]  w-[27.7vw] h-[13.84vw]  ">
+                        <div class="provide w-[27.7vw] h-[13.84vw] ">
+                            <div class="w-[27.7vw] pl-[5.76vw]  arrow tracking-[-3px] "><span
+                                    class="mr-[1.07vw]">Latest</span>最新
                             </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="w-[517px] h-[650px] ">
-                <div class="mt-[-65px]  w-[517px] h-[260px]  ">
-                    <div class="provide w-[517px] h-[260px] ">
-                        <div class="w-[517px] pl-[108px]  arrow tracking-[-3px] "><span class="mr-[20px]">Latest</span>最新
-                        </div>
-                        <div class=" w-[517px]  pl-[160.27px] mt-[-52px] flex gap-[15px] tracking-[-3px] row ">課程<span
-                                class="mt-[15px] tracking-[-3px]">course</span>
+                            <div class=" w-[27.7vw]  pl-[8.54vw] mt-[-2.78vw] flex gap-[0.81vw] tracking-[-3px] row ">
+                                課程<span class="mt-[0.81vw] tracking-[-3px]">course</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="w-[517px] h-[390px] mt-[51.7px] relative flex flex-col justify-end ">
-                    <transition name="slide-down">
-                        <div v-if="topCardIndex === 0" class="trans-title w-[517px]  h-[156px] title pl-[77.5px]  mb-[1px]">
-                            {{ cards[0].title }}</div>
-                        <div v-else-if="topCardIndex === 1" class="trans-title w-[517px]  h-[156px] title pl-[77.5px]  mb-[1px]">
-                            {{ cards[1].title }}</div>
-                        <div v-else-if="topCardIndex === 2" class="trans-title w-[517px]  h-[156px] title pl-[77.5px]  mb-[1px]">
-                            {{ cards[2].title }}</div>
-                        <div v-else-if="topCardIndex === 3" class="trans-title w-[517px]  h-[156px] title pl-[77.5px]  mb-[1px] ">
-                            {{ cards[3].title }}</div>
-                    </transition>
-                    <transition name="slide-down-content">
-                        <div v-if="topCardIndex === 0"
-                            class="trans-content w-[517px] h-[167px] intro pl-[77.5px] pt-[20.6px] mb-[1px]">{{ cards[0].text }}</div>
-                        <div v-else-if="topCardIndex === 1"
-                            class="trans-content w-[517px] h-[167px] intro pl-[77.5px] pt-[20.6px] mb-[1px]">{{ cards[1].text }}</div>
-                        <div v-else-if="topCardIndex === 2"
-                            class="trans-content w-[517px] h-[167px] intro pl-[77.5px] pt-[20.6px] mb-[1px]">{{ cards[2].text }}</div>
-                        <div v-else-if="topCardIndex === 3"
-                            class="trans-content w-[517px] h-[167px] intro pl-[77.5px] pt-[20.6px] mb-[1px] ">{{ cards[3].text }}
-                        </div>
-                    </transition>
-                    <transition name="slide-down-more">
-                        <div v-if="topCardIndex === 0" class="trans-more w-[517px] flex justify-end more "><a
-                                :href="cards[0].link">了解更多<i class="fa-solid fa-arrow-right"></i>
-                            </a></div>
-                        <div v-else-if="topCardIndex === 1" class="trans-more w-[517px] flex justify-end more "><a
-                                :href="cards[1].link">了解更多<i class="fa-solid fa-arrow-right"></i> </a></div>
-                        <div v-else-if="topCardIndex === 2" class="trans-more w-[517px] flex justify-end more "><a
-                                :href="cards[2].link">了解更多 <i class="fa-solid fa-arrow-right"></i> </a></div>
-                        <div v-else-if="topCardIndex === 3" class="trans-more w-[517px] flex justify-end more "><a
-                                :href="cards[3].link">了解更多 <i class="fa-solid fa-arrow-right"></i> </a></div>
-                    </transition>
-                </div>
+                    <div class="w-[26.92vw] h-[20.31vw] mt-[2.69vw] relative flex flex-col justify-end ">
+                        <transition name="slide-down">
+                            <div v-if="topCardIndex === 0"
+                                class="trans-title w-[27.79vw]  h-[8.38vw] title pl-[4.16vw]  mb-[0.053vw]">
+                                {{ cards[0].title }}</div>
+                            <div v-else-if="topCardIndex === 1"
+                                class="trans-title w-[27.79vw]  h-[8.38vw] title pl-[4.16vw]  mb-[0.053vw]">
+                                {{ cards[1].title }}</div>
+                            <div v-else-if="topCardIndex === 2"
+                                class="trans-title w-[27.79vw]  h-[8.38vw] title pl-[4.16vw]  mb-[0.053vw]">
+                                {{ cards[2].title }}</div>
+                            <div v-else-if="topCardIndex === 3"
+                                class="trans-title w-[27.79vw]  h-[8.38vw] title pl-[4.16vw]  mb-[0.053vw] ">
+                                {{ cards[3].title }}</div>
+                        </transition>
+                        <transition name="slide-down-content">
+                            <div v-if="topCardIndex === 0"
+                                class="trans-content w-[27.79vw] h-[8.97vw] intro pl-[4.16vw] pt-[1.107vw] mb-[0.053vw]">{{
+                                    cards[0].text }}</div>
+                            <div v-else-if="topCardIndex === 1"
+                                class="trans-content w-[27.79vw] h-[8.97vw] intro pl-[4.16vw] pt-[1.107vw] mb-[0.053vw]">{{
+                                    cards[1].text }}</div>
+                            <div v-else-if="topCardIndex === 2"
+                                class="trans-content w-[27.79vw] h-[8.97vw] intro pl-[4.16vw] pt-[1.107vw] mb-[0.053vw]">{{
+                                    cards[2].text }}</div>
+                            <div v-else-if="topCardIndex === 3"
+                                class="trans-content w-[27.79vw] h-[8.97vw] intro pl-[4.16vw] pt-[1.107vw] mb-[0.053vw] ">{{
+                                    cards[3].text }}
+                            </div>
+                        </transition>
+                        <transition name="slide-down-more">
+                            <div v-if="topCardIndex === 0" class="trans-more w-[26.9vw]  flex justify-end more "><a
+                                    :href="cards[0].link">了解更多<i class="fa-solid fa-arrow-right"></i>
+                                </a></div>
+                            <div v-else-if="topCardIndex === 1" class="trans-more w-[26.9vw] flex justify-end more "><a
+                                    :href="cards[1].link">了解更多<i class="fa-solid fa-arrow-right"></i> </a></div>
+                            <div v-else-if="topCardIndex === 2" class="trans-more w-[26.9vw] flex justify-end more "><a
+                                    :href="cards[2].link">了解更多 <i class="fa-solid fa-arrow-right"></i> </a></div>
+                            <div v-else-if="topCardIndex === 3" class="trans-more w-[26.9vw] flex justify-end more "><a
+                                    :href="cards[3].link">了解更多 <i class="fa-solid fa-arrow-right"></i> </a></div>
+                        </transition>
+                    </div>
 
-            </div>
-        </section>
+                </div>
+            </section>
+        </div>
     </div>
 </template>
 <style scoped>
@@ -120,15 +132,15 @@ export default {
 }
 
 .trans-title {
-    top: 39px;
+    top: 2.03vw;
 }
 
 .trans-content {
-    top: 202.8px;
+    top: 10.56vw;
 }
 
 .trans-more {
-    top: 358.8px;
+    top: 18.68vw;
 }
 
 /* transition-title */
@@ -142,12 +154,12 @@ export default {
 
 .slide-down-enter-from {
     opacity: 0;
-    transform: translateY(-50px);
+    transform: translateY(-2.6vw);
 }
 
 .slide-down-leave-to {
     opacity: 0;
-    transform: translateY(30px);
+    transform: translateY(1.56vw);
 }
 
 /* transition-content */
@@ -161,12 +173,12 @@ export default {
 
 .slide-down-content-enter-from {
     opacity: 0;
-    transform: translateY(-40px);
+    transform: translateY(-2.08vw);
 }
 
 .slide-down-content-leave-to {
     opacity: 0;
-    transform: translateY(30px);
+    transform: translateY(1.56vw);
 }
 
 /* transition-moreLink*/
@@ -180,12 +192,12 @@ export default {
 
 .slide-down-more-enter-from {
     opacity: 0;
-    transform: translateY(-40px);
+    transform: translateY(-2.08vw);
 }
 
 .slide-down-more-leave-to {
     opacity: 0;
-    transform: translateY(30px);
+    transform: translateY(1.56vw);
 }
 
 
@@ -193,7 +205,7 @@ export default {
 /* Card Styles */
 .card {
 
-    @apply w-[737px] h-[468px] p-[30px] absolute cursor-pointer border-dotted;
+    @apply w-[38.38vw] h-[24.37vw] p-[1.56vw] absolute cursor-pointer border-dotted;
     transition: 0.5s ease-in-out;
     transform-origin: bottom center;
     will-change: transform;
@@ -206,46 +218,46 @@ export default {
 }
 
 .card:nth-child(2) {
-    top: 60.84px;
+    top: 3.125vw;
     left: 0;
     /* background-color: #ffcc00; */
 }
 
 .card:nth-child(3) {
-    top: 121.68px;
+    top: 6.33vw;
     left: 0;
     /* background-color: #db5621 */
 }
 
 .card:nth-child(4) {
-    top: 182.52px;
+    top: 9.5vw;
     /* background-color: #7dbda8; */
 }
 
 /* Card Content Styles */
 .card-title {
-    @apply text-[#ffffff] text-[38px] font-bold;
+    @apply text-[#ffffff] text-[1.97vw] font-bold;
     font-family: 'Noto Sans TC', sans-serif;
 }
 
 .card-tag {
-    @apply bg-[#ffffff] text-[19px] mr-[10px] rounded-full px-[20px] py-[8px] font-bold mt-[15px];
+    @apply bg-[#ffffff] text-[0.98vw] mr-[0.52vw] rounded-full px-[1.04vw] py-[0.41vw] font-bold mt-[0.78vw];
     font-family: 'Inter', sans-serif;
 }
 
 .card-type {
-    @apply w-[139.6px] py-[7.76px] text-[19px] mb-[15.52px] text-white border-white border-2 border-solid rounded-full font-semibold flex justify-center items-center;
+    @apply w-[7.11vw] py-[0.40vw] text-[0.98vw] mb-[0.82vw] text-white border-white border-2 border-solid rounded-full font-semibold flex justify-center items-center;
     font-family: 'Inter', sans-serif;
 }
 
 /* Provide Styles */
 .provide {
-    @apply text-[95px] font-bold;
+    @apply text-[4.94vw] font-bold;
     font-family: 'Noto Sans TC', sans-serif;
 }
 
 .provide span {
-    @apply text-[50px] font-bold;
+    @apply text-[2.6vw] font-bold;
     font-family: 'Inter', sans-serif;
 }
 
@@ -255,30 +267,29 @@ export default {
 }
 
 .row:after {
-    @apply absolute top-[80.8px] left-[410px] w-[120px] h-[110px] bg-no-repeat bg-right bg-[length:85px_85px] rotate-90;
+    @apply absolute top-[4.34vw] left-[22.04vw] w-[6.45vw] h-[5.9vw] bg-no-repeat bg-right bg-[length:4.56vw_4.56vw] rotate-90;
     content: "";
     background-image: url('/images/banner/blue-row.png');
 }
 
 /* Title Styles */
 .title {
-    @apply text-[72px] text-[#0054ff] font-black;
+    @apply text-[3.75vw] text-[#0054ff] font-black;
     font-family: 'Noto Sans TC', sans-serif;
     line-height: 1.1;
 }
 
 /* Introduction Styles */
 .intro {
-    @apply text-[20px] font-bold;
+    @apply text-[1.04vw] font-bold;
     line-height: 1.6;
     font-family: 'Noto Sans TC', sans-serif;
 }
 
 /* More Styles */
 .more {
-    @apply text-[20px] font-extrabold text-[#0057ff];
+    @apply text-[1.04vw] font-extrabold text-[#0057ff];
     font-family: 'Inter', sans-serif;
-}
-</style>
+}</style>
 
 
