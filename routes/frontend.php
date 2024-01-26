56ebde73d6b29071fefddcb64de29473d68340b2
SwitchSu <152354849+SwitchSu@users.noreply.github.com>
2024-01-26 14:32:54 +0800

    create : Controller

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Frontend\AboutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\IndexController;
 
 Route::get('/', [IndexController::class, 'index'])->name('home');
+
+Route::get('/about',[AboutController::class,'index'])->name('About.index');
