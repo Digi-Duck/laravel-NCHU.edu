c824b88265c7d4f058028548a984a10b02571d7d
SwitchSu <simon93107@gmail.com>
2024-01-31 23:37:56 +0800

    create : motion

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import { createInertiaApp, Link, Head } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import { createPinia } from 'pinia';
+import { MotionPlugin } from '@vueuse/motion';
 
 const appName = import.meta.env.VITE_APP_NAME || '國立中興大學推廣教育組';
 const pinia = createPinia();
@@ -24,6 +25,7 @@ createInertiaApp({
       .use(plugin)
       .use(pinia)
       .use(ZiggyVue, Ziggy)
+      .use(MotionPlugin)
       .component('Link', Link)
       .component('Head', Head)
       .mount(el);
